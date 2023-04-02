d578bbedfc8ed108fea2bca5705f8baf557b7a9e
Nadia Yef <tester17hi@gmail.com>
2023-04-02 05:47:38 +0200

    homework to lection 5

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Birthday birthday = new Birthday();
+
+        int result = birthday.printAge();
+
+        System.out.println("~~~" + result + "~~~");
+    }
+}
